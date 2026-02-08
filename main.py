sirdog3355/EnduraBot

commit df73f48382345843a750b9511da5aed76976e9a1

enhance: Allow cmd disabling to have a reason

main.py
@@ -113,15 +113,18 @@ async def on_app_command_error(interaction: discord.Interaction, error: app_comm
             await interaction.response.send_message(embed=embed, ephemeral=True)
             return
 
-        elif interaction.command.name in SETTINGS_DATA["disabled_cmds"]:
+    cmds = SETTINGS_DATA["disabled_cmds"].items()
+    for name, reason in cmds:
+        if interaction.command.name == name:
 
-            logger.log(UNAUTHORIZED, f"{interaction.user.name} ({interaction.user.id}) attempted to run disabled command /{interaction.command.name}.")
+            logger.log(UNAUTHORIZED, f"{interaction.user.name} ({interaction.user.id}) attempted to run disabled command /{interaction.command.name}. Reason: [{reason}]")
 
             embed = discord.Embed(
                 title=":gear: Command disabled.",
-                description=f"Command `/{interaction.command.name}` has been disabled.",
+                description=f"`/{interaction.command.name}` has been disabled by a systems operator. Review the field below for more details.",
                 color=8650752
             )
+            embed.add_field(name="Reason", value=reason)
             await interaction.response.send_message(embed=embed, ephemeral=True)
             return
         

utils/permissions_checker.py
@@ -16,8 +16,10 @@ async def check_permissions(interaction: discord.Interaction):
         return False
 
     # If command is disabled, reject.
-    if interaction.command.name in SETTINGS_DATA["disabled_cmds"]:
-        return False
+    cmds = SETTINGS_DATA["disabled_cmds"].items()
+    for name, reason in cmds:
+        if interaction.command.name == name:
+            return False
     
     # If no IDs exist, then the command doesn't have a restriction.
     if not eligible_role_ids:

commit 1d234903ead98ea403005b44171d59d77f9666d6

bug: Fix cmd disabling issue introduced in commit [df73f48] / PR 230

main.py
@@ -95,7 +95,14 @@ async def on_ready():
 
 @bot.tree.error
 async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
-    
+
+    cmds = SETTINGS_DATA["disabled_cmds"].items()
+    disabled_cmds = []
+
+    if not disabled_cmds:
+        for name, reason in cmds:
+            disabled_cmds.append(name)
+
     if interaction.response.is_done(): 
         return
     
@@ -113,18 +120,16 @@ async def on_app_command_error(interaction: discord.Interaction, error: app_comm
             await interaction.response.send_message(embed=embed, ephemeral=True)
             return
 
-    cmds = SETTINGS_DATA["disabled_cmds"].items()
-    for name, reason in cmds:
-        if interaction.command.name == name:
+        if interaction.command.name in disabled_cmds:
 
             logger.log(UNAUTHORIZED, f"{interaction.user.name} ({interaction.user.id}) attempted to run disabled command /{interaction.command.name}. Reason: [{reason}]")
 
             embed = discord.Embed(
                 title=":gear: Command disabled.",
-                description=f"`/{interaction.command.name}` has been disabled by a systems operator. Review the field below for more details.",
+                description=f"`/{interaction.command.name}` has been disabled by a systems operator.",
                 color=8650752
             )
-            embed.add_field(name="Reason", value=reason)
+            embed.add_field(name="Reason Provided by Operator", value=reason)
             await interaction.response.send_message(embed=embed, ephemeral=True)
             return
         